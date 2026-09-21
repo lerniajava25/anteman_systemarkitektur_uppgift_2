9952163fa8de2f02f64ce6474fc7121887d3884a
Anders Eriksson <anteman.swe@gmail.com>
2026-09-21 19:54:05 +0200

    Remove `uppgift2` package and related classes; update project configuration files.

--- src/main/java/uppgift2/Computer.java
+++
@@ -1,9 +0,0 @@
-package uppgift2;
-
-public interface Computer {
-    void turnOn();
-    void turnOff();
-    void restart();
-    void printCPU();
-    boolean status();
-}
--- src/main/java/uppgift2/Processor.java
+++
@@ -1,7 +0,0 @@
-package uppgift2;
-
-public interface Processor {
-    int getClockSpeedMhz();
-    int getCacheL1();
-    String getCPU();
-}
